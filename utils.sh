116dba974e8044b146522685e2459920fef36560
Piotr Kalinowski <piotr.kalinowski@chmurowisko.pl>
2021-12-14 11:22:06 +0100

    utils updated

--- utils.sh
+++ utils.sh
@@ -9,3 +9,8 @@ usermod -a -G docker ubuntu
 
 # az cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
